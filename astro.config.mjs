e33232cf82d0159899b0ae1d8c3b86f9d8af6922
oscark22 <98506040+oscark22@users.noreply.github.com>
2023-05-14 18:15:38 -0700

    refactoring and markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    drafts: true
+  },
+  integrations: [mdx({
+    drafts: true
+  })]
+});
